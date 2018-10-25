6c58b4ff8ec3a8c5ff918c3a0dbed665d4a78ed9
ridream97 <merculova1997@mail.ru>
2018-10-25 11:32:19 +0300

    settings.py config

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,6 +72,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
